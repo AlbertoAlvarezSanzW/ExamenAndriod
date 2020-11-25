d1d6d7067afd42530f4a53b53855b978fc595fe5
Alberto <Alberto.alvarez.sanz@gmail.com>
2020-11-25 13:39:22 +0100

    commit

--- app/src/main/java/com/example/examen/MainActivity.kt
+++ app/src/main/java/com/example/examen/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.examen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 
@@ -13,27 +15,42 @@ sigue en la 2 hoja
  */
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val EJ1 = "VARIABLE1"
+    }
 
-    companion object{const val variableCompartida="variableEstatica"}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val edit1 = findViewById<EditText>(R.id.editT)
+        val boton1 = findViewById<Button>(R.id.button)
+        boton1.setEnabled(false)
 
-        // creamos el edit text como constante
-        val EditText = findViewById<EditText>(R.id.Edit2)
-
-        // creamos funcionalidad al boton
-        val boton = findViewById<Button>(R.id.boton)
-
-        // damos funcionalidad al usuario:
-        boton.setOnClickListener{
-            val intent = Intent(this,segundaActividad::class.java)
-            intent.putExtra(variableCompartida,1)
+        edit1.addTextChangedListener(object: TextWatcher {
+            override fun onTextChanged(s:CharSequence, start:Int, before:Int, count:Int) {
+                var w:Int =0
+                for (i in s) {
+                    if (s.isBlank())
+                        w++
+                }
+                if (w==4) {
+                    boton1.setEnabled(true)
+                } else {
+                    boton1.setEnabled(false)
+                }
+            }
+            override fun beforeTextChanged(s:CharSequence, start:Int, count:Int,after:Int) {
+            }
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+        boton1.setOnClickListener {
+            val intent =  Intent ( this, SecondActivity :: class .java)
+            intent.putExtra(EJ1,1)
+            startActivity (intent)
         }
-
-
 
     }
 }
--- app/src/main/java/com/example/examen/SecondActivity.kt
+++ app/src/main/java/com/example/examen/SecondActivity.kt
@@ -1,15 +1,27 @@
 package com.example.examen
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class segundaActividad : AppCompatActivity()  {
+class SecondActivity : AppCompatActivity()  {
+
+    companion object{const val variableCompartida="variableEstatica"}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.segundaactividad)
+
+        // tenemos que recibirlo con una constante que es val
+        val number = 0
+        val variable = intent.getIntExtra(variableCompartida,number)
+        val texto = findViewById<TextView>(R.id.textView2)
+        // esto es asi, xq es asi y .
+        variable?.let {
+            texto.text = variable.toString()
+        }
 
         val textView = findViewById<TextView>(R.id.textView2)
         val boton1 = findViewById<Button>(R.id.boton1)
@@ -19,5 +31,8 @@ class segundaActividad : AppCompatActivity()  {
 
 
 
+
     }
 }
+
+
