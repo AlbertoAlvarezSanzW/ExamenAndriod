2d93b6f47d00099b85ba0efda423bfb1cbf801ce
Alberto <Alberto.alvarez.sanz@gmail.com>
2020-11-25 12:38:21 +0100

    commit

--- app/src/main/java/com/example/examen/MainActivity.kt
+++ app/src/main/java/com/example/examen/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.examen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 
 /*
@@ -18,6 +20,15 @@ class MainActivity : AppCompatActivity() {
 
         // creamos el edit text como constante
         val EditText = findViewById<EditText>(R.id.Edit2)
+
+        // creamos funcionalidad al boton
+        val boton = findViewById<Button>(R.id.boton)
+
+        // damos funcionalidad al usuario:
+        boton.setOnClickListener{
+            val intent = Intent(this,segundaActividad::class.java)
+
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/examen/segundaActividad.kt
@@ -0,0 +1,14 @@
+package com.example.examen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class segundaActividad : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.segundaactividad)
+
+
+
+    }
+}
